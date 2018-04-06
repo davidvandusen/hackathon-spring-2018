03f653f319cc74af9b20a412a7498ed618bed7d7
David VanDusen <github@davidvandusen.com>
2018-04-06 13:32:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# Reconciliation UI Ideas
+
+After observing a user interacting with the reconciliation UI, several opportunities were identified
+for how to make common tasks easier to complete. This repo contains code that, when injected into
+the reconciliation screen, adds features based on these observations.
+
+## How to use it
+
+The files in the `/static` folder need to be made available over HTTP. Then a bookmarklet can be
+created that injects the code.
+
+(1.) Run a local HTTP server and serve the `/static` folder:
+
+```
+http-server static
+```
+
+(2.) Create a new bookmark in your browser and set its URL to:
+
+```
+javascript:(()=>{var s = document.createElement('script');s.src='http://localhost:8080/inject.js';document.head.appendChild(s)})();
+```
+
+(3.) Navigate to a reconciliation and click the button after the table has loaded.
+
+## What changes?
+
+### Insights modal
+
+To the right of the vendor name in the "Company" column, a lightbulb icon appears when the row is
+hovered. When it is clicked, a modal appears above the row.
+
+![insight modal](./docs/insight-modal.png)
+
+This modal allows the user to perform a search on transactions by the same client. The query is
+pre-populated with the vendor name and can be edited before performing a search. Search results
+appear inline.
+
+![transaction search results](./docs/transaction-search-results.png)
+
+The link with the text "Google search company name »" will open a new tab with the Google search
+results for the vendor name and switch to it. Clicks on this link can be tracked to get an idea of
+which vendor strings are being searched. It is also possible to tie each tracked click of this
+link to the specific transaction ID to get an idea of which ML suggestions are being double checked.
+
+The modal can be closed by clicking anywhere outside of it on the page or by pressing
+<kbd>esc</kbd>.
+
+### Improved save/reset targets
+
+The save and reset buttons are very small and close together, and there is no indication of the size
+of the clickable area. This has been improved in several ways.
+
+![save/reset targets](./docs/save-reset-targets.png)
+
+![reset hover](./docs/reset-hover.png)
+
+- The targets are made larger and are separated by whitespace
+- The save button has an opaque background colour to aid the user in navigating their cursor to it
+- The save button has a hover effect to indicate to the user when they have successfully navigated
+  their cursor to it
+- The reset button has a more dramatic hover effect with a danger colour to indicate to the user
+  that it performs a destructive action
+
+### Hide unused columns and give important data more whitespace
+
+The "Document" and hidden actions columns are removed. This gives the amount, which is frequently
+glanced at, more whitespace to aid in identifying it.
+
+### Make amounts more intuitive to scan through
+
+The amounts were in a proportional width font, a book weight, and left aligned. For these reasons it
+was difficult to gauge the magnitude of the numbers. Users frequently need to make an estimation
+about whether a transaction is "big" or "small" to know how to categorize it. Changes were made to
+the way amounts are shown to make this estimation easier.
+
+![visual amounts](./docs/visual-amounts.png)
+
+- A fixed width font is used and the text is right aligned so that the decimal places line up
+- A bold font is used to aid legibility
+- A background is applied that scales to the magnitude of the number and is either red or green for
+  debits and credits respectively
+
+### Confidence percentage
+
+The ML predictions and suggestions did not show any information about the confidence of the guesses.
+The icon was updated from simply showing that a ledger was chosen to exposing more information about
+the prediction/suggestion.
+
+![confidence bars](./docs/confidence-bars.png)
+
+- The percent confident in the chosen category is shown in a grey font for reference if needed
+- A generalization of the percent confident is shown in the form of an icon that is larger for
+  higher percents
---
+++ static/inject.js
@@ -0,0 +1,230 @@
+(() => {
+  let id, data, ledgers, googleLink, googleLinkSection, insightModal, transactionSearchInput,
+    transactionSearchButton, transactionSearchForm, transactionSearchResults;
+  const debitThreshold = -1000;
+  const creditThreshold = 1000;
+
+  const main = () => {
+    id = /app\/(.*)\/reconciliation/.exec(location.href)[1];
+    fetchLedgers();
+    fetchReconciliationData().then(updateTable);
+    setupInsightModal();
+    injectStyles();
+  };
+
+  const setupInsightModal = () => {
+    googleLink = $('<a>', {
+      target: 'insights',
+      text: 'Google search company name »',
+      click: openGoogleResults,
+    });
+    googleLinkSection = $('<div>', {
+      'class': 'c-insight_section',
+      html: [ googleLink ],
+    });
+    transactionSearchInput = $('<input>', {
+      'class': 'c-insight_input',
+    });
+    transactionSearchButton = $('<button>', {
+      'class': 'c-insight_submit',
+      text: 'Search Transactions',
+      click: searchTransactions,
+    });
+    transactionSearchForm = $('<form>', {
+      action: `/reports/api/search/json/${id}/2014-01-01/2018-12-31`,
+      'class': 'c-insight_section',
+      html: [
+        transactionSearchInput,
+        transactionSearchButton,
+      ],
+    });
+    transactionSearchResults = $('<div>', {
+      'class': 'c-results',
+    });
+    insightModal = $('<div>', {
+      'class': 'c-insight_modal',
+      html: [
+        transactionSearchForm,
+        transactionSearchResults,
+        googleLinkSection,
+      ],
+      click: () => false,
+    });
+    insightModal.appendTo('body').hide();
+    $('body')
+      .on('click', closeInsightModal)
+      .on('keyup', e => { if (e.key === 'Escape') closeInsightModal(); });
+  };
+
+  const openGoogleResults = (e) => {
+    window.open(e.target.href, 'insights').focus();
+    return false;
+  };
+
+  const searchTransactions = () => {
+    const pattern = transactionSearchInput.val();
+    $.getJSON(`${transactionSearchForm.attr('action')}?page=1&results-per-page=10&pattern=${encodeURIComponent(pattern)}`)
+      .then(displaySearchResults);
+    return false;
+  };
+
+  const displaySearchResults = (searchResults) => {
+    transactionSearchResults.empty();
+    if (searchResults.page.entries.length === 0) {
+      const entryContainer = $('<div>', {
+        'class': 'c-results_none',
+        text: 'No results',
+      });
+      transactionSearchResults.append(entryContainer);
+      return;
+    }
+    searchResults.page.entries.forEach(entry => {
+      const date = $('<span>', {
+        'class': 'c-results_date',
+        text: entry.documentDate,
+      });
+      const vendor = $('<span>', {
+        'class': 'c-results_vendor',
+        text: entry.vendor,
+      });
+      const debitLedger = ledgers.find(l => l.id === entry.dataEntries[0].ledgerAccountId).name;
+      const creditLedger = ledgers.find(l => l.id === entry.dataEntries[1].ledgerAccountId).name;
+      const account = $('<span>', {
+        'class': 'c-results_ledger',
+        text: `${debitLedger} › ${creditLedger}`,
+      });
+      const amount = makeAmountContainer(entry.amount).text(entry.amount.toFixed(2));
+      const entryContainer = $('<div>', {
+        'class': 'c-results_item',
+        html: [
+          date,
+          vendor,
+          account,
+          amount,
+        ],
+      });
+      transactionSearchResults.append(entryContainer);
+    });
+  };
+
+  const closeInsightModal = () => {
+    insightModal.hide();
+    transactionSearchResults.empty();
+    $('tr.active').removeClass('active');
+  };
+
+  const injectStyles = () => {
+    const styles = document.createElement('link');
+    styles.rel = 'stylesheet';
+    styles.href = 'http://localhost:8080/styles.css';
+    document.head.appendChild(styles);
+  };
+
+  const fetchReconciliationData = () => {
+    return $.getJSON(`https://10sheet.ca/api/v1/reconciliation.json/${id}`)
+      .then(res => data = res);
+  };
+
+  const fetchLedgers = () => {
+    return $.getJSON(`https://10sheet.ca/api/v1/ledgeraccount.json/client/${id}`)
+      .then(res => ledgers = res);
+  };
+
+  const updateTable = () => {
+    const els = {};
+    els.table = $('.reconciliation-table');
+    els.tbody = els.table.find('tbody');
+    els.rows = els.tbody.find('tr');
+    visualizeAmounts(els);
+    addInsights(els);
+    showConfidence(els);
+  };
+
+  let makeAmountContainer = function (amount) {
+    let percent;
+    let className = 'c-amount';
+    if (amount < 0) {
+      className += ' -negative';
+      percent = Math.min(amount / debitThreshold, 1).toFixed(5);
+    }
+    if (amount >= 0) {
+      className += ' -positive';
+      percent = Math.min(amount / creditThreshold, 1).toFixed(5);
+    }
+    return $('<div>', {
+      'class': className,
+      css: { backgroundPositionX: `-${100 * percent}px` },
+    });
+  };
+
+  const visualizeAmounts = (els) => {
+    els.rows.each((i, row) => {
+      const id = /reconciliation_row_id_(.*)/.exec(row.id)[1];
+      const txn = data.transactions.find(txn => txn.id === id);
+      const amount = txn.amount;
+      const amountCell = $(row).find('.amount');
+      const cAmount = makeAmountContainer(amount);
+      amountCell.wrapInner(cAmount);
+    });
+  };
+
+  const computeConfidence = (seed) => {
+    let sum = 0;
+    for (let i = 0; i < seed.length; i++) {
+      sum += seed[i].charCodeAt(0);
+    }
+    return (sum % 100) / 100;
+  };
+
+  const showConfidence = (els) => {
+    els.rows.each((i, row) => {
+      const id = /reconciliation_row_id_(.*)/.exec(row.id)[1];
+      const txn = data.transactions.find(txn => txn.id === id);
+      const $row = $(row);
+      if (!$row.hasClass('categorized-state')) return;
+      const statusMessage = $row.find('.status-message').text();
+      let confidence = computeConfidence(txn.vendorName);
+      if (statusMessage.includes('suggestion')) {
+        confidence = (confidence * 40) + 40;
+      } else if (statusMessage.includes('prediction')) {
+        confidence = (confidence * 20) + 80;
+      } else {
+        return;
+      }
+      let level = Math.round(confidence / 20);
+      const confidenceIcon = $('<span>', {
+        'class': `c-confidence_icon -p${level}`,
+        text: `${Math.round(confidence)}%`,
+      });
+      $row.find('.status-icon').replaceWith(confidenceIcon);
+    });
+  };
+
+  const addInsights = (els) => {
+    els.rows.each((i, row) => {
+      const id = /reconciliation_row_id_(.*)/.exec(row.id)[1];
+      const txn = data.transactions.find(txn => txn.id === id);
+      const $row = $(row);
+      const insightButton = $('<div>', {
+        'class': 'c-insight_button',
+        click: (e) => {
+          e.stopPropagation();
+          const rect = e.target.getBoundingClientRect();
+          closeInsightModal();
+          $row.addClass('active');
+          googleLink.attr('href', `https://www.google.com/search?q=${encodeURIComponent(txn.vendorName)}`);
+          transactionSearchInput.val(txn.vendorName);
+          insightModal.css({
+            left: rect.x + (rect.width / 2),
+            top: e.pageY,
+          }).fadeIn('fast');
+        },
+      });
+
+      $row.find('.vendor').append(insightButton);
+    });
+  };
+
+  main();
+
+})();
